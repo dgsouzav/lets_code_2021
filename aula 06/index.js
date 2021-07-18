4fadc2052665925b4c9d14bf8db389999375d232
Diogo Souza <diogouba11@gmail.com>
2021-07-18 19:59:39 -0300

    update index.js

---
+++ aula 06/index.js
@@ -0,0 +1,19 @@
+// while
+let count = 0; 
+while(count < 100){
+    console.log(count);
+    count++;
+}
+
+// do-while
+let count = 0; 
+do{
+    console.log(count);
+    count++;
+}
+while(count < 100);
+
+// for
+for(let i = 0; i < 10; i++){
+    console.log(i+1);
+}
